53560721f9cf2be9b63aaeb9fd43a41f720fca14
Mikel Garcia <mikel_6@hotmail.com>
2017-08-25 14:17:16 +0200

    First approach with coinbin

--- App.js
+++ App.js
@@ -1,23 +1,106 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 
-export default class App extends React.Component {
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  groupContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderBottomColor: '#eee',
+    borderBottomWidth: 1,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+  },
+  groupName: {
+    fontWeight: 'bold',
+    flex: 0.7,
+  },
+});
+
+class Currency extends Component {
   render() {
+    const { name, rank, symbol, usd } = this.props.currency;
+
+    return (
+      <TouchableHighlight
+        key={rank}
+      >
+        <View style={styles.groupContainer}>
+          <Text style={styles.groupName}>{`${symbol} ${name}`}</Text>
+          <Text style={styles.groupName}>{`${usd}`}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+Currency.propTypes = {
+  currency: PropTypes.shape({
+    name: PropTypes.string,
+    rank: PropTypes.number,
+    symbol: PropTypes.string,
+    usd: PropTypes.number,
+  }),
+};
+
+class Currencies extends Component {
+  static navigationOptions = {
+    title: 'Currencies',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: [],
+    };
+  };
+
+  componentWillMount() {
+    return fetch('https://coinbin.org/coins')
+      .then((response) => response.json())
+      .then(({coins = {}}) => {
+        const cryptos = [];
+        Object.keys(coins).forEach((coin) => {
+          if (coins[coin].rank < 32) {
+            cryptos.push(Object.assign(coins[coin], { symbol: coin }));
+          }
+        });
+
+        this.setState({ dataSource: cryptos.sort((a, b) => a.rank - b.rank) });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  keyExtractor = item => item.rank;
+
+  renderItem = ({ item }) => <Currency currency={item} />;
+
+  render() {
+    const { dataSource } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList
+          data={dataSource.slice(0, 31)}
+          keyExtractor={this.keyExtractor}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Currencies;
