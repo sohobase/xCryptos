f9c6d79d8ff60a82f7bff2d5da443ae923accc15
@soyjavi <javi.jimenez.villar@gmail.com>
2017-09-17 12:05:27 +0700

    First approach creating alarms (redux-only)

--- src/config/theme.js
+++ src/config/theme.js
@@ -5,26 +5,25 @@ const RED = '#FF5C84';
 const BLUE = '#2283CD';
 
 export default {
-  // -- Base
+  // -- Transitions
+  ANIMATION_DURATION: 400,
+  ANIMATION_EASING: 'ease-in-out-sine',
+
+  // -- Colors
   WHITE: 'white',
   BLACK: 'black',
-  // -- Colors
   PRIMARY: '#3920F5',
   ACCENT: '#C668FD',
   CONTRAST: 'rgba(255, 255, 255, 0.75)',
   COLOR_LOW: 'rgba(255,0,0,0.35)',
   COLOR_HIGH: 'rgba(0,255,0,0.35)',
   COLOR_COINBASE: '#0667D0',
+  FONT_PRIMARY_COLOR: 'black',
 
   TRANSPARENT: 'transparent',
   BACKGROUND_HIGHLIGHT: 'rgba(255, 255, 255, 0.1)',
   BACKGROUND_DARK_HIGHLIGHT: 'rgba(0, 0, 0, 0.1)',
-
-  // -- Sizes
-  UNIT,
-  OFFSET: UNIT * 1.6,
-  MAIN_LAYOUT_HEIGHT: '55%',
-  SECOND_LAYOUT_HEIGHT: '45%',
+  MODAL_BACKDROP: 'rgba(0, 0, 0, 0.5)',
 
   // -- Fonts
   FONT_SIZE_SMALL: UNIT * 1.2,
@@ -33,4 +32,10 @@ export default {
   FONT_SIZE_EXTRA_LARGE: UNIT * 2.4,
   FONT_WEIGHT_LIGHT: '200',
   FONT_WEIGHT_BOLD: '700',
+
+  // -- Sizes
+  UNIT,
+  OFFSET: UNIT * 1.6,
+  MAIN_LAYOUT_HEIGHT: '55%',
+  SECOND_LAYOUT_HEIGHT: '45%',
 };
--- src/screens/AlertsScreen.js
+++ src/screens/AlertsScreen.js
@@ -1,12 +1,11 @@
 import { arrayOf, func } from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button, FlatList, Text, View } from 'react-native';
-import PopupDialog, { SlideAnimation } from 'react-native-popup-dialog';
-
+import { FlatList, View } from 'react-native';
+import { FormLabel, FormInput } from 'react-native-elements';
 import { addAlertAction, removeAlertAction } from '../actions';
-import { C, STYLE } from '../config';
-import { AlertListItem, ButtonIcon } from '../components';
+import { C, STYLE, THEME } from '../config';
+import { AlertListItem, Button, ButtonIcon, Modal } from '../components';
 import styles from './AlertsScreen.style';
 
 const keyExtractor = ({ currency, low, high }) => `${currency}${low}${high}${new Date()}`;
@@ -17,61 +16,101 @@ class AlertsScreen extends Component {
 
     return {
       title: `${currency.name} Alerts`,
-      // headerRight: <ButtonIcon icon="add" onPress={() => console.log(this) } />,
+      headerRight: <ButtonIcon icon="add" onPress={() => {}} />,
     };
   }
 
   constructor(props) {
     super(props);
-
-    this._addAlert = this._addAlert.bind(this);
-    this._removeAlert = this._removeAlert.bind(this);
+    this.state = {
+      item: undefined,
+      modal: true,
+    };
+    this._closeAlert = this._closeAlert.bind(this);
+    this._onChangeAmount = this._onChangeAmount.bind(this);
     this._renderItem = this._renderItem.bind(this);
+    this._saveAlert = this._saveAlert.bind(this);
+    this._showAlert = this._showAlert.bind(this);
   }
 
-  _addAlert() {
-    const { addAlert } = this.props;
-    addAlert();
+  _closeAlert() {
+    this.setState({ modal: false, item: undefined });
   }
 
-  _removeAlert(item) {
-    // const { removeAlert } = this.props;
-    console.log('>>', this, item);
-    // removeAlert();
+  _saveAlert() {
+    const { _closeAlert } = this;
+    const { addAlert, removeAlert, currency: { symbol } } = this.props;
+    const { item = {} } = this.state;
+
+    if (item.currency) removeAlert(item);
+    else addAlert({ ...item, currency: symbol });
+    _closeAlert();
+  }
+
+  _showAlert(item) {
+    this.setState({ item, modal: false });
+  }
+
+  _onChangeAmount(field, value) {
+    const { item = {} } = this.state;
+
+    this.setState({
+      item: { ...item, [field]: parseFloat(value) },
+    });
   }
 
   _renderItem({ item }) {
-    const { _removeAlert } = this;
-    // const { popupDialog: { show } } = this;
-
-    // console.log('renderItem>>>>', item, _removeAlert, this);
+    const { _showAlert } = this;
     return (
-      <AlertListItem
-        alert={item}
-        onPress={_removeAlert}
-      />
+      <AlertListItem alert={item} onPress={() => _showAlert(item)} />
     );
   }
 
   render() {
-    const { _renderItem, dialog } = this;
+    const { _closeAlert, _onChangeAmount, _renderItem, _saveAlert } = this;
     const { alerts } = this.props;
+    const { item, modal } = this.state;
+
+    const inputProps = {
+      containerStyle: styles.fieldReset,
+      keyboardType: 'numeric',
+      underlineColorAndroid: THEME.PRIMARY,
+    };
 
     return (
       <View style={STYLE.SCREEN}>
-        <Button
-          title="Show Dialog"
-          onPress={() => dialog.show()}
-        />
-        <PopupDialog
-          dialogAnimation={new SlideAnimation({ slideFrom: 'bottom' })}
-          ref={(popupDialog) => { this.dialog = popupDialog; }}
+        <Modal
+          title={`${modal ? 'New' : 'Edit'} alert`}
+          onClose={_closeAlert}
+          visible={modal || item !== undefined}
         >
-          <View>
-            <Text>Hello world</Text>
+          <View style={[STYLE.ROW]}>
+            <View style={styles.fieldset}>
+              <FormLabel labelStyle={[styles.fieldReset]}>Low</FormLabel>
+              <FormInput
+                autoFocus
+                defaultValue={item && item.low && item.low.toString()}
+                inputStyle={styles.input}
+                onChangeText={_onChangeAmount.bind(null, 'low')} //eslint-disable-line
+                ref={input => this.low = input} //eslint-disable-line
+                {...inputProps}
+              />
+            </View>
+            <View style={styles.fieldset}>
+              <FormLabel labelStyle={[styles.fieldReset, styles.labelRight]}>High</FormLabel>
+              <FormInput
+                defaultValue={item && item.high && item.high.toString()}
+                inputStyle={[styles.input, styles.inputRight]}
+                onChangeText={_onChangeAmount.bind(null, 'high')} //eslint-disable-line
+                ref={input => this.high = input} //eslint-disable-line
+                {...inputProps}
+              />
+            </View>
           </View>
-        </PopupDialog>
-
+          <Button caption="Save" onPress={() => { _saveAlert(); }} style={STYLE.MODAL_BUTTON} />
+          { item && item.currency &&
+            <Button caption="Delete" onPress={() => { _saveAlert(); }} style={STYLE.MODAL_BUTTON} /> }
+        </Modal>
         <FlatList data={alerts} keyExtractor={keyExtractor} renderItem={_renderItem} />
       </View>
     );
@@ -81,18 +120,25 @@ class AlertsScreen extends Component {
 AlertsScreen.propTypes = {
   addAlert: func,
   alerts: arrayOf(C.SHAPE.ALERT),
+  currency: C.SHAPE.CURRENCY,
   removeAlert: func,
 };
 
 AlertsScreen.defaultProps = {
   addAlert() {},
   alerts: [],
+  currency: undefined,
   removeAlert() {},
 };
 
-const mapStateToProps = state => ({
-  alerts: state.alerts,
-});
+const mapStateToProps = ({ alerts = [] }, props) => {
+  const { currency = {} } = props.navigation.state.params;
+
+  return {
+    currency,
+    alerts: alerts.filter(item => currency.symbol === item.currency),
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
   addAlert: alert => dispatch(addAlertAction(alert)),
