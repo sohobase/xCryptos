9b2dab58f3b558475ebbf90ede03ae6fb013bc31
@soyjavi <javi.jimenez.villar@gmail.com>
2017-09-18 19:25:51 +0700

    #refacto notifications

--- src/services/notifications.js
+++ src/services/notifications.js
@@ -1,17 +1,17 @@
 import { Permissions, Notifications } from 'expo';
 
+const { NOTIFICATIONS } = Permissions;
+const GRANTED = 'granted';
+
 export default async function registerForPushNotificationsAsync() {
-  const { existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+  const { existingStatus } = await Permissions.getAsync(NOTIFICATIONS);
   let finalStatus = existingStatus;
 
-  if (existingStatus !== 'granted') {
-    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+  if (existingStatus !== GRANTED) {
+    const { status } = await Permissions.askAsync(NOTIFICATIONS);
     finalStatus = status;
   }
-
-  if (finalStatus !== 'granted') {
-    return undefined;
-  }
+  if (finalStatus !== GRANTED) return undefined;
 
   const token = await Notifications.getExpoPushTokenAsync();
 
