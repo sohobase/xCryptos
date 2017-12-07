12a9a230f501b3177b40a2670ac467056e459c1b
@soyjavi <javi.jimenez.villar@gmail.com>
2017-12-07 12:08:35 +0700

    #bugfixed header in android

--- src/app.style.js
+++ src/app.style.js
@@ -1,20 +1,16 @@
-import { Platform, StatusBar, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { THEME } from './config';
 
-let androidHeader = {};
-if (Platform.OS !== 'ios') androidHeader = { paddingTop: StatusBar.currentHeight, height: 80 };
-
 export default StyleSheet.create({
-  header: Object.assign(
-    {
-      backgroundColor: THEME.PRIMARY,
-      elevation: 0,
-      shadowColor: 'transparent',
-      shadowRadius: 0,
-      shadowOffset: {
-        height: 0,
-      },
-    }, androidHeader),
+  header: {
+    backgroundColor: THEME.PRIMARY,
+    elevation: 0,
+    shadowColor: 'transparent',
+    shadowRadius: 0,
+    shadowOffset: {
+      height: 0,
+    },
+  },
 
   title: {
     alignSelf: 'center',
