64154345db4d236b1c243f35122f98bba730baab
@soyjavi <javi.jimenez.villar@gmail.com>
2018-01-10 14:35:16 +0700

    #improvement show the current value of chart

--- src/screens/Main/components/Chart.js
+++ src/screens/Main/components/Chart.js
@@ -1,5 +1,5 @@
 import { arrayOf, bool, func, shape, string } from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { View as Motion } from 'react-native-animatable';
 import { Touchable } from '../../../components';
@@ -20,71 +20,100 @@ const Option = ({
   </Touchable>
 );
 
-const Chart = ({
-  coin: { price }, fetching, dataSource = [], onTimeline, onValue, timeline,
-}) => {
-  let max = 0;
-  let min = 0;
-  let trend;
-  if (!fetching && dataSource.length > 0) {
-    max = Math.max.apply(null, dataSource.map(({ value }) => value));
-    min = Math.min.apply(null, dataSource.map(({ value }) => value));
-    trend = (dataSource.find(({ value }) => value === max || value === min)).value === max;
+class Chart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      price: undefined,
+      timestamp: (new Date()).getTime(),
+    };
+    this._onValue = this._onValue.bind(this);
   }
-  const diff = max - min;
 
-  return (
-    <View style={styles.container}>
-      <View style={[STYLE.ROW, styles.prices, trend && styles.reverse]}>
-        <ChipPrice price={price} value={min} context="low" />
-        <View style={[STYLE.CENTERED, styles.space]}>
-          <ChipPrice value={price} />
+  componentWillReceiveProps() {
+    this.setState({ timestamp: (new Date()).getTime() });
+  }
+
+  _onValue(price) {
+    this.setState({ price });
+  }
+
+  render() {
+    const {
+      _onValue,
+      props: {
+        coin, fetching, dataSource = [], onTimeline, timeline,
+      },
+      state: { price = coin.price, timestamp },
+    } = this;
+
+    let max = 0;
+    let min = 0;
+    let trend;
+    if (!fetching && dataSource.length > 0) {
+      max = Math.max.apply(null, dataSource.map(({ value }) => value));
+      min = Math.min.apply(null, dataSource.map(({ value }) => value));
+      trend = (dataSource.find(({ value }) => value === max || value === min)).value === max;
+    }
+    const diff = max - min;
+
+    return (
+      <View style={styles.container}>
+        <View style={[STYLE.ROW, styles.prices, trend && styles.reverse]}>
+          <ChipPrice price={price} value={min} context="low" />
+          <View style={[STYLE.CENTERED, styles.space]}>
+            <ChipPrice value={price} />
+          </View>
+          <ChipPrice price={price} value={max} context="high" />
         </View>
-        <ChipPrice price={price} value={max} context="high" />
-      </View>
-      <View style={[STYLE.ROW, styles.bars]}>
-        {
-          dataSource.map(({ value }, index) => {
-            let color = COLOR.CHART;
-            if (value === min) color = COLOR.LOW;
-            if (value === max) color = COLOR.HIGH;
-            const key = `${timeline}-${new Date()}-${index}`;
-            const height = fetching ? 0 : ((value - min) * 100) / diff;
+        <View style={[STYLE.ROW, styles.bars]}>
+          {
+            dataSource.map(({ value }, index) => {
+              let color = COLOR.CHART;
+              if (value === min) color = COLOR.LOW;
+              if (value === max) color = COLOR.HIGH;
+              const key = `${timeline}-${timestamp}-${index}`;
+              const height = fetching ? 0 : ((value - min) * 100) / diff;
 
-            return (
-              <TouchableOpacity key={key} onPressIn={() => onValue(value)} onPressOut={onValue} style={styles.bar}>
-                <Motion
-                  {...MOTION.DEFAULT}
-                  delay={index * 5}
-                  style={[styles.value, { height: `${height}%`, backgroundColor: color }]}
-                />
-              </TouchableOpacity>
-            );
-          })
-        }
+              return (
+                <TouchableOpacity
+                  key={key}
+                  onPressIn={() => _onValue(value)}
+                  onPressOut={() => _onValue()}
+                  style={styles.bar}
+                >
+                  <Motion
+                    {...MOTION.DEFAULT}
+                    delay={index * 5}
+                    style={[styles.value, { height: `${height}%`, backgroundColor: color }]}
+                  />
+                </TouchableOpacity>
+              );
+            })
+          }
+        </View>
+        <View style={STYLE.ROW}>
+          {
+            TIMELINES.map(key => (
+              <Option
+                active={key === timeline}
+                caption={key}
+                key={key}
+                onPress={() => !fetching && onTimeline(key)}
+              />
+            ))
+          }
+        </View>
       </View>
-      <View style={STYLE.ROW}>
-        {
-          TIMELINES.map(key => (
-            <Option
-              active={key === timeline}
-              caption={key}
-              key={key}
-              onPress={() => !fetching && onTimeline(key)}
-            />
-          ))
-        }
-      </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 Chart.propTypes = {
   coin: shape(SHAPE.COIN),
   fetching: bool,
   dataSource: arrayOf(shape(SHAPE.HISTORY)),
   onTimeline: func,
-  onValue: func,
   timeline: string,
 };
 
@@ -93,7 +122,6 @@ Chart.defaultProps = {
   fetching: false,
   dataSource: [],
   onTimeline() {},
-  onValue() {},
   timeline: undefined,
 };
 
